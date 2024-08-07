a5bf0e0f03b7ed42345b243fc7f9ab043b939413
Dmytro Balytskyi <dmytro.balytskyi@gmail.com>
2024-08-07 12:32:58 +0300

    Правка завантаження файлу схеми schema.graphql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { createServer } from 'node:http'
 import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 import { createYoga, createSchema } from 'graphql-yoga'
 import { resolvers } from './resolvers.js'
 import config from './config.js'
 
-var typeDefs = readFileSync('./src/schema.graphql', 'utf-8')
+var __filename = fileURLToPath(import.meta.url)
+var __dirname = path.dirname(__filename)
+var schemaPath = path.join(__dirname, 'schema.graphql');
+
+var typeDefs = readFileSync(schemaPath, 'utf-8')
 
 var yoga = createYoga({
   schema: createSchema({
